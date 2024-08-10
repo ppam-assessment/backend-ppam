d37cb8faa60161c27224ddf4b53afd1565d32696
veda <vedabezaleel@gmail.com>
2024-08-11 01:50:11 +0700

    ref: instrument type choice

--- api/instrument/controller.ts
+++ api/instrument/controller.ts
@@ -10,10 +10,51 @@ export async function getAllInstrumentController(req: FastifyRequest, res: Fasti
 
     const instruments = await getAllInstrument({ from: fromNumber, to: toNumber });
     const result = instruments.map(instrument => {
-        const { id, number, topicId, question, type, choice, sub } = instrument;
+        const { id, number, topicId, question, choice, sub } = instrument;
+        let {type} = instrument;
         let caseShape = {};
         switch (type) {
             case 'select':
+            case 'selectya':
+                caseShape = {
+                    choice: [
+                        {
+                            value: 'Ya',
+                            code: 1
+                        },
+                        {
+                            value: 'Tidak',
+                            code: 2
+                        },
+                        {
+                            value: 'Tidak Tahu',
+                            code: 3
+                        },
+                    ]
+                }
+                type = 'select'
+                break;
+
+            case 'selectideal':
+                caseShape = {
+                    choice: [
+                        {
+                            value: 'Ideal',
+                            code: 1
+                        },
+                        {
+                            value: 'Minimal Diperlukan',
+                            code: 2
+                        },
+                        {
+                            value: 'Tidak Memadai',
+                            code: 3
+                        },
+                    ]
+                }
+                type = 'select'
+                break;
+
             case 'multiselect':
                 caseShape = {
                     choice

df9f49cf99c769eaec245c084c7d5e7939f8405b
veda <vedabezaleel@gmail.com>
2024-08-10 16:34:39 +0700

    chore: delete instrument schema

--- api/instrument/route.ts
+++ api/instrument/route.ts
@@ -1,7 +1,5 @@
 import { FastifyInstance } from "fastify";
 import { getAllInstrumentController } from "./controller.js";
-// import { createUserController, loginController } from "./controller.js";
-// import { $ref } from "./schema.js";
 
 export default function (app: FastifyInstance, opts: any, done: any) {
 

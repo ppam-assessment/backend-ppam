4520788faaf0cf75aeef4db3937d6ae3ae8d3eff
veda <vedabezaleel@gmail.com>
2024-08-10 19:32:20 +0700

    feat: input route

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import userRoutes from "./user/route.js";
 import instrumentRoutes from "./instrument/route.js"
+import responseRoutes from "./response/route.js"
 
 export default function (app: any, opts: any, done: any) {
   app.get("/", async function testHandler(req: FastifyRequest, res: FastifyReply) {
@@ -12,6 +13,7 @@ export default function (app: any, opts: any, done: any) {
 
   app.register(userRoutes, { prefix: '/account' })
   app.register(instrumentRoutes, { prefix: '/instrument' })
+  app.register(responseRoutes, { prefix: '/response' })
 
   done();
 }

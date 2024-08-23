cc20551e4a04262f09238e46fa122cde33b5b2b7
veda <vedabezaleel@gmail.com>
2024-08-23 15:50:40 +0700

    chore: remove dist directory

--- dist/src/api/instrument/route.js
+++
@@ -1,6 +0,0 @@
-import { getAllInstrumentController, getAreaController } from "./controller.js";
-export default function (app, opts, done) {
-    app.get("/", getAllInstrumentController);
-    app.get("/area", getAreaController);
-    done();
-}
--- dist/src/utils/lib/timePeriod.js
+++
@@ -1,3 +0,0 @@
-export default async function getNextDay() {
-    return new Date(Date.now() + 1000 * 60 * 60 * 24);
-}

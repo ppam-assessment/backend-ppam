731a4591beb1c9e728bb056397b14a24e076df7a
veda <vedabezaleel@gmail.com>
2024-08-23 15:49:20 +0700

    chore: make new prisma client instance for seeding

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,12 +1,12 @@
-import prisma from "../../config/prisma.js"
+import prisma from "../client.js"
 import topics from "./data/topic.js"
 import instruments from "./data/instrument.js"
 
 async function main() {
 
-  // await prisma.topics.createMany({
-  //   data: topics
-  // })
+  await prisma.topics.createMany({
+    data: topics
+  })
 
   for (const instrument of instruments) {
     await prisma.instrument.create({

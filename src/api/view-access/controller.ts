cc3023f50b390046d02c33f43cbf5c7673220e49
veda <vedabezaleel@gmail.com>
2024-09-01 04:18:22 +0700

    feat: add error throw when viewer access already created in db

--- src/api/view-access/controller.ts
+++ src/api/view-access/controller.ts
@@ -1,17 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { getSessionUser } from "../../service/prisma/session.js";
 import { Status } from "@prisma/client";
-import { createViewerAccess } from "../../service/prisma/viewerAccess.js";
+import { createViewerAccess, readViewerAccessByUserId } from "../../service/prisma/viewerAccess.js";
 import { PostViewerAccessSchema } from "./schema.js";
 
 export const postViewerAccessController = async (req: FastifyRequest, res: FastifyReply) => {
     const session = await req.jwtVerify() as TokenPayload
     const { user } = await getSessionUser({ id: session.id })
 
+    const access = await readViewerAccessByUserId({userId: user.id})
+
     if (!user) {
         return Error("User not found.");
     } else if (user.status !== Status.viewer) {
         return Error("User doesn't have access.");
+    } else if (access) {
+        return Error(`View access already created for ${user.username}.`)
     }
 
     const createdAccess = await createViewerAccess({ userId: user.id });
@@ -20,3 +24,4 @@ export const postViewerAccessController = async (req: FastifyRequest, res: Fasti
         data: createdAccess
     })
 }
+

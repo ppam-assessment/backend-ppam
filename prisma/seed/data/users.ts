ef75de758a3abd75753dc6f5ba6b1dbf1d0ba023
veda <vedabezaleel@gmail.com>
2024-09-02 21:20:26 +0700

    fix: user seed attribute institutes to institute

--- prisma/seed/data/users.ts
+++ prisma/seed/data/users.ts
@@ -12,7 +12,7 @@ export default [
         id: 'test-submitter',
         username: 'test',
         email: 'developer1@gmail.com',
-        institutes: 'test',
+        institute: 'test',
         password: '$2a$12$2K55OKAqhjd7v5ey40MNsOgzgCZVYLIwjtTugD4Qb6X3.8ojCKax6', // code maniac
         status: Status.submitter
     }

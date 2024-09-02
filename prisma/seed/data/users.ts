a0e30206df1ec438805d772ba469239a4b9fc174
veda <vedabezaleel@gmail.com>
2024-09-02 21:31:10 +0700

    temp: bacl

--- prisma/seed/data/users.ts
+++ prisma/seed/data/users.ts
@@ -2,35 +2,18 @@ import { Status } from "@prisma/client";
 
 export default [
     {
-      id: '0c873de8-bd3a-41e6-b3dc-3d5c41064d85',
-      username: 'Rafidha Nur Alifah',
-      institute: 'Universitas Negeri Semarang',
-      password: '$2b$10$ouCfKw.20J3GozsrbDw7oOa02X8oG3Er6OzXYFnodPVQSE97i5bW2',
-      email: 'rafidhaalifah28@gmail.com',
-      status: Status.submitter,
+        id: 'test-admin',
+        username: 'developer',
+        email: 'developer@gmail.com',
+        password: '$2a$12$2K55OKAqhjd7v5ey40MNsOgzgCZVYLIwjtTugD4Qb6X3.8ojCKax6', // code maniac
+        status: Status.admin
     },
     {
-      id: '869bc8d5-1593-46df-9a6b-30f0eb653307',
-      username: 'Efa Nugroho',
-      institute: 'Universitas Negeri Semarang',
-      password: '$2b$10$5DJzv8cvLnaxTQta858OOuqkt21dVd2LYTiFhRmcbuQM2f8UyY7Mm',
-      email: 'efa.nugroho@mail.unnes.ac.id',
-      status: Status.submitter,
-    },
-    {
-      id: '222f089f-5081-441d-82ac-4bd021d8eebe',
-      username: 'Ayu Istiada',
-      institute: 'Dinas Kesehatan Kabupaten Magelang',
-      password: '$2b$10$TZPftJD4CI8KgiYcuLc8meVzgjsTJ1s466IxbAPUaqYM0N.Om2RUi',
-      email: 'istiadasr@gmail.com',
-      status: Status.submitter,
-    },
-    {
-      id: 'ad28f970-d775-4c45-b596-3b4644f2a168',
-      username: 'Efa',
-      institute: 'UNNES',
-      password: '$2b$10$/B/YTjTjKIE8VU2ITGGzqu3PNYkbeJ1m7vck8ocxR.5aOKOLZBAdi',
-      email: 'efa@gmail.com',
-      status: Status.submitter,
-    },
-  ]
+        id: 'test-submitter',
+        username: 'test',
+        email: 'developer1@gmail.com',
+        institute: 'test',
+        password: '$2a$12$2K55OKAqhjd7v5ey40MNsOgzgCZVYLIwjtTugD4Qb6X3.8ojCKax6', // code maniac
+        status: Status.submitter
+    }
+]

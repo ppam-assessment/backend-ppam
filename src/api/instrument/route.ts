20b5f349ef98b74d50a06e4fe4e9da4de91dd1f9
veda <vedabezaleel@gmail.com>
2024-10-03 16:31:07 +0700

    feat: edit instrument question for admin

--- src/api/instrument/route.ts
+++ src/api/instrument/route.ts
@@ -1,10 +1,17 @@
 import { FastifyInstance } from "fastify";
-import { getAllInstrumentController, getAreaController } from "./controller.js";
+import { getAllInstrumentController, getAreaController, putInstrumentsQuestionController } from "./controller.js";
+import { $ref } from "./schema.js";
 
 export default function (app: FastifyInstance, opts: any, done: any) {
 
   app.get("/", getAllInstrumentController)
+  app.put("/", {
+    schema: {
+      body: $ref('putInstrumentsSchema'),
+    }
+  }, putInstrumentsQuestionController)
+
   app.get("/area", getAreaController)
-  
+
   done();
 }
